630cbee4964bebb6e6051401def82a4143e8147f
TaariniSarath <taarini97@gmail.com>
2016-11-18 15:55:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,11 @@
 console.log('Loaded!');
 
 var element=document.getElementById('main-text');
+
 element.innerHTML = 'New Value';
+
+var img= document.getElementById("img");
+img.onclick = function()
+{
+    img.style.marginleft = '100px';
+};
